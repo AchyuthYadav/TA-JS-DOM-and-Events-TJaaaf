543505e81e2b23a6a0d990d0db752f2101dfc434
achyuth yadav <achyuthyadav@hmail.com>
2022-03-25 11:51:26 +0530

    assignment

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,20 +1,30 @@
 
 let first = document.querySelector(".first")
 
-let second = document.querySelector("second")
-
-let ban = document.querySelector("banner")
-
-let ban1 = document.querySelector("banner1")
+let second = document.querySelector(".second")
 
 
-first.addEventListener("click" , function ( element ) {
-   let currentColor = element.style.background;
-    if (currentColor == "red"){
-        element.style.background= "green"
-    } else {
-        element.style.background= "red"
-    }ppp
-})
 
+
+
+function random(ele) {
+    var a = (Math.random() * 256);
+    var b = (Math.random() * 256);
+    var c = (Math.random() * 256); 
+    
+    var randomColor = "rgb("+a+" , "+b+"  , "+c+")";
+    ele.style.backgroundColor = randomColor;
+}
+
+
+first.addEventListener("click", function(){
+    return random(first);
+});
   
+
+second.addEventListener("mousemove", function(){
+    return random(second);
+});
+
+    
+   

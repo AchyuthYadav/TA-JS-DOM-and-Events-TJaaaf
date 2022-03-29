7b2e23b219c4a77b448572d423eded4ec4585511
achyuth yadav <achyuthyadav@hmail.com>
2022-03-29 11:11:38 +0530

    assignment

---
+++ block-BJaaep/code/script.js
@@ -0,0 +1,14 @@
+
+
+let root = document.querySelector(".wrapper")
+let ul = document.querySelector(".boxes")
+let li = document.querySelector(".box")
+
+boxes.forEach(element => {
+    let box = document.createElement("div")
+    div.classList.add("box")
+});
+
+
+
+

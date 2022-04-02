33d87d8cb7c65e533c2d0ad2706e01b1e87521c9
achyuth yadav <achyuthyadav@hmail.com>
2022-04-02 15:36:27 +0530

    assignment

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,5 +1,5 @@
 let form = document.querySelector("form");
-let model = document.querySelector(".modal__overlay");
+let model = document.querySelector(".modelinfo");
 
 form.addEventListener("submit", (Event) => {
     Event.preventDefault();
@@ -8,21 +8,25 @@ form.addEventListener("submit", (Event) => {
     userData.email=(Event.target.elements.email.value);
     userData.choice=(Event.target.elements.choice.value);
     userData.color=(Event.target.elements.color.value);
-    userData.movie=(Event.target.elements.movie.value);
-    userData.book=(Event.target.elements.book.value);
-    userData.terms=(Event.target.elements.terms.value);
+    userData.range=(Event.target.elements.range.value);
+    userData.drone=(Event.target.elements.drone.value);
+    userData.terms=(Event.target.elements.terms.checked);
 
     console.log(userData)
 
-    modal.classlist.add("open");
-
-    let close = document.querySelector(".modal__overlay")
-    close.addEventListener("click" , () => {
-        modal.classlist.remove("open")
-    })
+   
     
 });
 
 // to insert  data we have to use variable
 
 let userData = {}
+
+function display(data, {}) {
+    h1 = document.createElement("h1");
+    h1.innerText = "hello ${data.name}";
+
+    modelinfo.append(h1)
+
+}
+
